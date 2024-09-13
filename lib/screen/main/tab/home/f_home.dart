2f5bf5f4bb99b1665019e8909c85d6672f830d92
itbebop <it.bebop@gmail.com>
2024-09-13 16:35:30 +0900

    feat: scroll controller로 버튼 크기 변경

--- lib/screen/main/tab/home/f_home.dart
+++ lib/screen/main/tab/home/f_home.dart
@@ -2,18 +2,49 @@ import 'package:fast_app_base/common/common.dart';
 import 'package:fast_app_base/common/widget/round_button_theme.dart';
 import 'package:fast_app_base/common/widget/w_round_button.dart';
 import 'package:fast_app_base/screen/dialog/d_message.dart';
+import 'package:fast_app_base/screen/main/daangn/w_floating_daagn_button.riverpod.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../../../dialog/d_color_bottom.dart';
 import '../../../dialog/d_confirm.dart';
 
-class HomeFragment extends StatelessWidget {
+class HomeFragment extends ConsumerStatefulWidget {
   const HomeFragment({
     Key? key,
   }) : super(key: key);
 
   @override
+  ConsumerState<ConsumerStatefulWidget> createState() => _HomeFragmentState();
+}
+
+class _HomeFragmentState extends ConsumerState<HomeFragment> {
+  final scrollController = ScrollController();
+
+  @override
+  void initState() {
+    scrollController.addListener(() {
+      final isSmall = ref.read(FloatingActionButtonIsSmallProvider);
+
+      if (scrollController.position.pixels > 100 && !isSmall) {
+        ref.read(FloatingActionButtonIsSmallProvider.notifier).state = true;
+      } else if (scrollController.position.pixels < 100 && isSmall) {
+        ref.read(FloatingActionButtonIsSmallProvider.notifier).state = false;
+      }
+    });
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return ListView(
+      controller: scrollController,
+      children: [
+        Container(height: 500, color: Colors.red),
+        Container(height: 500, color: Colors.blue),
+        Container(height: 500, color: Colors.green),
+        Container(height: 500, color: Colors.yellow),
+      ],
+    );
   }
 }
