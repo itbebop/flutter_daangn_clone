e77c9f4303ddf9f2914e58722c5acede567d9ae4
itbebop <it.bebop@gmail.com>
2024-09-14 12:19:31 +0900

    feat: button state 변경 방법2(정석)

--- lib/screen/main/daangn/w_floating_daagn_button.riverpod.dart
+++ lib/screen/main/daangn/w_floating_daagn_button.riverpod.dart
@@ -1,17 +1,9 @@
+import 'package:fast_app_base/screen/main/daangn/w_floating_daangn_button.state.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-
-class FloatingButtonState {
-  bool isExpanded;
-  bool isSmall;
-
-  FloatingButtonState()
-      : isExpanded = false,
-        isSmall = false;
-}
 
 final floatingButtonStateProvider = StateNotifierProvider<FloatingButtonStateNotifier, FloatingButtonState>(
   (ref) => FloatingButtonStateNotifier(
-    FloatingButtonState(),
+    const FloatingButtonState(false, false),
   ),
 );
 
@@ -21,18 +13,27 @@ class FloatingButtonStateNotifier extends StateNotifier<FloatingButtonState> {
   @override
   bool updateShouldNotify(old, current) {
     // statte에 할당할 때 넘겨주는 새로운 state와 기존의 value에 들어있는 state를 비교하는 함수
-    // return super.updateShouldNotify(old, current);
-    return true; // 무조건 바뀌었다고 알려주는 것(편법)
+    return super.updateShouldNotify(old, current); // 같지 않다면 true
+    // 이렇게 쓰려면 실제로 다른 값이 내려오게 해야함
+    // return true; // 무조건 바뀌었다고 알려주는 것(편법)
   }
 
   void onTapButton() {
-    state = state
-      ..isExpanded = !state.isExpanded
-      ..isSmall = true;
+    // state = state
+    //   ..isExpanded = !state.isExpanded
+    //   ..isSmall = true;
+
+    // 다른 객체를 넣어줌
+    // freezed 만들어서 copywith도 쓸 수 있지만
+    // 값 2개다 바꿔야하므로 그냥 이렇게 만들어줌
+    state = FloatingButtonState(!state.isExpanded, true);
   }
 
   void changeButtonSize(bool isSmall) {
-    print(isSmall);
-    state = state..isSmall = isSmall;
+    // state = state..isSmall = isSmall;
+
+    // state = FloatingButtonState(state.isExpanded, isSmall);
+
+    state = state.copyWith(isSmall: isSmall);
   }
 }
--- lib/screen/main/tab/local_life/f_local_life.dart
+++ lib/screen/main/tab/local_life/f_local_life.dart
@@ -1,15 +1,43 @@
+import 'package:fast_app_base/screen/main/daangn/w_floating_daagn_button.riverpod.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class LocalLifeFragment extends StatefulWidget {
-  const LocalLifeFragment({super.key});
+class LocalLifeFragment extends ConsumerStatefulWidget {
+  const LocalLifeFragment({
+    Key? key,
+  }) : super(key: key);
 
   @override
-  State<LocalLifeFragment> createState() => _LocalLifeFragmentState();
+  ConsumerState<ConsumerStatefulWidget> createState() => _LocalLifeFragmentState();
 }
 
-class _LocalLifeFragmentState extends State<LocalLifeFragment> {
+class _LocalLifeFragmentState extends ConsumerState<LocalLifeFragment> {
+  final scrollController = ScrollController();
+
+  @override
+  void initState() {
+    scrollController.addListener(() {
+      final floatingState = ref.read(floatingButtonStateProvider);
+
+      if (scrollController.position.pixels > 100 && !floatingState.isSmall) {
+        ref.read(floatingButtonStateProvider.notifier).changeButtonSize(true);
+      } else if (scrollController.position.pixels < 100 && floatingState.isSmall) {
+        ref.read(floatingButtonStateProvider.notifier).changeButtonSize(false);
+      }
+    });
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return ListView(
+      controller: scrollController,
+      children: [
+        Container(height: 500, color: Colors.orange),
+        Container(height: 500, color: Colors.blueAccent),
+        Container(height: 500, color: Colors.amber[200]),
+        Container(height: 500, color: Colors.blueGrey),
+      ],
+    );
   }
 }
