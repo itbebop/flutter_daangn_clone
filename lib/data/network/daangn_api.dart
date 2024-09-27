231087b3c8f2f9fcb803ab3a48e6d1b228890660
itbebop <it.bebop@gmail.com>
2024-09-27 14:31:01 +0900

    feat: 3초로 api 시간 변경, 데이터 요청부분

--- lib/data/network/daangn_api.dart
+++ lib/data/network/daangn_api.dart
@@ -7,7 +7,7 @@ import 'package:fast_app_base/screen/notification/vo/vo_notification.dart';
 class DaangnApi {
   // 성공하면 List<DaangnNotification>, 실패하면 ApiError 타입의 결과 반환하도록
   static Future<SimpleResult<List<DaangnNotification>, ApiError>> getNotification() async {
-    await sleepAsync(300.ms);
+    await sleepAsync(3000.ms);
     return SimpleResult.success(notificationList);
     // 에러 반환하진 않음
     // 일반적으론 Singleton, 편의를 위해서 static함수로 만듬
--- lib/screen/notification/f_notification_list.dart
+++ lib/screen/notification/f_notification_list.dart
@@ -1,3 +1,4 @@
+import 'package:fast_app_base/screen/notification/provider/notification_provider.dart';
 import 'package:fast_app_base/screen/notification/vo/notification_dummies.dart';
 import 'package:fast_app_base/screen/notification/w_notification_item.dart';
 import 'package:flutter/material.dart';
@@ -9,8 +10,12 @@ class NotificationFragment extends HookConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return ListView(
-      children: notificationList.map((e) => NotificationItemWidget(onTap: () {}, notification: e)).toList(),
-    );
+    final list = ref.watch(notificationProvider);
+
+    return list == null
+        ? const Center(child: CircularProgressIndicator())
+        : ListView(
+            children: notificationList.map((e) => NotificationItemWidget(onTap: () {}, notification: e)).toList(),
+          );
   }
 }
