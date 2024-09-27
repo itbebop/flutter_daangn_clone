376d5f8a99f39ac552733e614b8b9e83a122c3ed
itbebop <it.bebop@gmail.com>
2024-09-27 15:07:38 +0900

    feat: 게시글 상세 정보 보여줄 model, api 생성

--- lib/data/network/daangn_api.dart
+++ lib/data/network/daangn_api.dart
@@ -1,6 +1,8 @@
 import 'package:fast_app_base/common/cli_common.dart';
 import 'package:fast_app_base/data/network/result/api_error.dart';
 import 'package:fast_app_base/data/simple_result.dart';
+import 'package:fast_app_base/entity/dummies.dart';
+import 'package:fast_app_base/entity/post/vo_product_post.dart';
 import 'package:fast_app_base/screen/notification/vo/notification_dummies.dart';
 import 'package:fast_app_base/screen/notification/vo/vo_notification.dart';
 
@@ -11,5 +13,10 @@ class DaangnApi {
     return SimpleResult.success(notificationList);
     // 에러 반환하진 않음
     // 일반적으론 Singleton, 편의를 위해서 static함수로 만듬
+  }
+
+  static Future<SimpleResult<ProductPost, ApiError>> getPost(int id) async {
+    await sleepAsync(500.ms);
+    return SimpleResult.success(ProductPost(simpleProductPost: post1, content: '깨끗하게 잘 쓰던 물건이에요'));
   }
 }
