cb321157262b364b628de8d769539871ad67f765
itbebop <it.bebop@gmail.com>
2024-10-02 21:25:53 +0900

    feat: detail data 가져오는 방법 2가지 - provider 내에 저장하도록 - 호출할 때마다 서버와 통신

--- lib/screen/post_detail/provider/product_post_provider.dart
+++ lib/screen/post_detail/provider/product_post_provider.dart
@@ -2,7 +2,19 @@ import 'package:fast_app_base/data/network/daangn_api.dart';
 import 'package:fast_app_base/entity/post/vo_product_post.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-// Provider : 데이터를 제공하는 역할
-final productPostProvider = FutureProviderFamily<ProductPost, int>((ref, id) async {
-  return await DaangnApi.getPost(id);
+// 데이터를 provider에 저장함
+// final productPostProvider = FutureProviderFamily<ProductPost, int>((ref, id) async {
+//   return await DaangnApi.getPost(id);
+// });
+
+// detail에서 호출할 때마다 서버와 통신
+class ProductPostNotifier extends AutoDisposeFamilyAsyncNotifier<ProductPost, int> {
+  @override
+  Future<ProductPost> build(int id) async {
+    return await DaangnApi.getPost(id);
+  }
+}
+
+final productPostProvider = AutoDisposeAsyncNotifierProviderFamily<ProductPostNotifier, ProductPost, int>(() {
+  return ProductPostNotifier();
 });
--- lib/screen/post_detail/s_post_detail.dart
+++ lib/screen/post_detail/s_post_detail.dart
@@ -1,18 +1,27 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:fast_app_base/common/common.dart';
 import 'package:fast_app_base/entity/post/vo_simple_product_post.dart';
+import 'package:fast_app_base/screen/post_detail/provider/product_post_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class PostDetailScreen extends ConsumerWidget {
   final SimpleProductPost? simpleProductPost;
+  final int id;
 
   const PostDetailScreen(
-    int id, {
+    this.id, {
     super.key,
     this.simpleProductPost,
   });
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return const Placeholder();
+    final productPost = ref.watch(productPostProvider(id)); // AsyncValue<ProductPost> productPost 타입
+    return productPost.when(
+      data: (data) => CachedNetworkImage(imageUrl: data.simpleProductPost.product.images[0]),
+      error: (error, trace) => '에러발생'.text.make(),
+      loading: () => const Center(child: CircularProgressIndicator()),
+    );
   }
 }
